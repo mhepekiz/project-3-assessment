3e659c955d8e9bf91eade14fd055d8c7359011ab
Mustafa Hepekiz <mustafah@Mustafas-MacBook-Pro-2.local>
2020-09-25 14:43:57 -0500

    Complete Step 3.2

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
+
 urlpatterns = [
-    path('', views.WackyCreate.as_view(), name='index'),
+    path('', views.index, name='index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,11 @@
-from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.shortcuts import render, redirect
 from .models import Wacky
-# Create your views here.
-def index(request):
-    wackies = Wacky.objects.all()
-    return render(request, 'index.html', {'wackies': wackies})
 
-class WackyCreate(CreateView):
-    model = Wacky
-    fields = '__all__'
+# Create your views here.
+
+
+def index(request):
+    wacky_list = Wacky.objects.all()
+  
+    return render(request, 'index.html', {'wacky_list': wacky_list})
 
