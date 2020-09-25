938b6a007a279520080e9f2dec24c2ae7a7e6b8a
Mustafa Hepekiz <mustafah@Mustafas-MacBook-Pro-2.local>
2020-09-25 14:46:35 -0500

    Complete Step 3.4

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,1 +1,9 @@
+from django import forms
+from django.forms import ModelForm
+from .models import Wacky
 
+
+class WackyForm(ModelForm):
+    class Meta:
+        model = Wacky
+        fields = '__all__'
--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render, redirect
 from .models import Wacky
+from .forms import WackyForm
 
 # Create your views here.
 
 
 def index(request):
     wacky_list = Wacky.objects.all()
+    wacky_form = WackyForm()
   
-    return render(request, 'index.html', {'wacky_list': wacky_list})
+    return render(request, 'index.html', {'wacky_list': wacky_list, 'wacky_form': wacky_form,
+    })
+
+def add_wacky(request):
+    form = WackyForm(request.POST)
+    if form.is_valid():
+        add_wacky = form.save()
+    return redirect('/')
 
